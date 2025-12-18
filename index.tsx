2aa2858d1f23cd95b1f5b92e01e8cc37fdb546af
efal <32618426+efal@users.noreply.github.com>
2025-12-18 22:44:26 +0100

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -18,8 +18,40 @@ const updateSW = registerSW({
   },
 });
 
+class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean, error: Error | null }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  public state: { hasError: boolean, error: Error | null };
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="p-4 text-red-500 bg-gray-900 h-screen">
+          <h1 className="text-xl font-bold mb-2">Something went wrong</h1>
+          <pre className="whitespace-pre-wrap font-mono text-sm bg-black p-4 rounded border border-red-900">
+            {this.state.error?.toString()}
+            {'\n\n'}
+            {this.state.error?.stack}
+          </pre>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
